ed5fdf961f09348ac446c7852b28ea919e0e79be
GauriVJ <gvjadhav039@gmail.com>
2024-02-25 12:34:30 +0530

    initial commit for React tutorial

---
+++ my-app/src/index.js
@@ -0,0 +1,25 @@
+// const h1= document.createElement('h1');
+// h1.innerHTML = 'I am learning React Js'
+
+// document.getElementById("root").appendChild(h1);
+
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+
+
+//content to show,
+//where to show,
+//callback)
+
+ReactDOM.render(
+  <>
+<h1>I am learning raect<br></br></h1>
+<h2>this is h2</h2>
+<h2>this is h2</h2>
+<h2>this is h2</h2>
+<h2>this is h2</h2>
+<h2>this is h2</h2>
+<h2>this is h2</h2>
+</>, 
+document.getElementById("root")
+);
